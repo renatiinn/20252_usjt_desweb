9ad11d030b6a941fa8c17aae62018f1308a0b6b5
Renato Peduto <repeduto@gmail.com>
2025-09-04 21:52:00 -0300

    apostila04(git-react) implementa as bootstrap

---
+++ ap04-react-props/src/App.jsx
@@ -0,0 +1,39 @@
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <i className="fa-hippo fa-2x fa-solid"></i>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-6 col-lg-3">
+          <div className="card">
+            <div className="card-header text-muted">
+              03/09/2025
+            </div>
+            <div className="card-body d-flex">
+              <i className="fa-solid fa-car fa-2x"></i>
+              <div>
+                <p>Carro</p>
+                <p>Porsche Amarelo</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-3">
+
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-3">
+
+        </div>
+        <div className="col-sm-12 col-md-6 col-lg-3">
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
